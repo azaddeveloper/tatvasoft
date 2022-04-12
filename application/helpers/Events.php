a8e4c14b2b3e348453390169c5ee2fc0ea3fc667
shubham azad <azaddeveloper>
2022-04-12 16:04:30 +0530

    rename file

--- application/helpers/Events.php
+++
@@ -1,9 +0,0 @@
-<?php
-function getDaysBetweenTwoDays($date1,$date2){
-  $diff = abs(strtotime($date2) - strtotime($date1));
-  $years = floor($diff / (365*60*60*24));
-  $months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
- return $days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
-}
-
-?>
